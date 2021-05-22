c84316f8127b1f18b170066ce492649111514b17
Jonathan Aguilar <aguilarj81496@@gmail.com>
2021-05-21 21:09:14 -0500

    accessed data base and added library

---
+++ mssqlDemo/Domain/Supplier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Domain
+{
+    class Supplier
+    {
+        public Supplier(int id, string businessName, string address, string phoneNumber)
+        {
+            Id = id;
+            BusinessName = businessName;
+            Address = address;
+            PhoneNumber = phoneNumber;
+
+        }
+
+        public Supplier(SqlDataReader dataReader)
+        {
+            // Check if reader hasnumber of columns that are expected
+            if (dataReader.FieldCount != 4)
+            {
+                throw new ArgumentException("Expected field count of 4");
+            }
+
+            Id = dataReader.GetInt32(dataReader.GetOrdinal("Id"));
+            
+        }
+
+        public int Id { get; set; }
+        public string BusinessName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+
+
+    }
+}
--- mssqlDemo/mssqlDemo/Program.cs
+++ mssqlDemo/mssqlDemo/Program.cs
@@ -5,32 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
+
 namespace mssqlDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("I'm gonna do database stuff");
             using (SqlConnection connection = new SqlConnection())
             {
-                connection.ConnectionString = "Data Source=localhost;Initial Catalog=my_playground;Integrated Security=True";
+                // TODO - In production we might go to lengths to hide our password, but this is just a learning exercise
+                connection.ConnectionString = "Data Source=localhost;Initial Catalog=mssqldemo;Persist Security Info=True;User ID=csharpapp;Password=csharpapp";
 
                 connection.Open();
-                Console.WriteLine("Succesfully Connected to Database!!! IM NOT DUMB");
-                SqlCommand command = new SqlCommand("SELECT * FROM Products", connection);
+                Console.WriteLine("Succesfully Connected to Database!!!");
+                SqlCommand command = new SqlCommand("SELECT * FROM Suppliers", connection);
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
+                    Console.WriteLine(string.Format("There are {0} columns", reader.FieldCount));
+                    
+                    
+
+
                     // while there is another record present
+                    int count = 0;
                     while (reader.Read())
                     {
-                        Console.WriteLine(string.Format("There are {0} columns", reader.FieldCount));    
-                        Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3} \t", reader[0], reader[1], reader[2], reader[3]));
+                        Console.WriteLine(String.Format("{0} | {1} | {2} | {3}", reader[0].ToString().Trim(), reader[1].ToString().Trim(), reader[2].ToString().Trim(), reader[3].ToString().Trim()));
+                        count++;
                     }
+                    Console.WriteLine(String.Format("There were {0} rows",count));
                 }
                 connection.Close();
             }
-            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Press 'ENTER' to continue...");
             Console.ReadLine();
         }
     }
